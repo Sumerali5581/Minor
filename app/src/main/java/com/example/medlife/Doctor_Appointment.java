8a004223d228a60445e173d14677264a0e69d258
Sumerali5581 <sumersmr7@gmail.com>
2023-05-04 00:20:42 +0530

    Commited

---
+++ app/src/main/java/com/example/medlife/Doctor_Appointment.java
@@ -0,0 +1,14 @@
+package com.example.medlife;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Doctor_Appointment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_doctor_appointment);
+    }
+}
---
+++ app/src/main/java/com/example/medlife/Patient_Appointment.java
@@ -0,0 +1,14 @@
+package com.example.medlife;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Patient_Appointment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_patient_appointment);
+    }
+}
