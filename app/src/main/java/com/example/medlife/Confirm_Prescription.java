f7bf788c07cbe4ce6f6bee951f9007090ec42444
Sumerali5581 <sumersmr7@gmail.com>
2023-05-04 23:57:56 +0530

    Commited

---
+++ app/src/main/java/com/example/medlife/Confirm_Prescription.java
@@ -0,0 +1,14 @@
+package com.example.medlife;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Confirm_Prescription extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_confirm_prescription);
+    }
+}
